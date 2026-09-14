c8bd41cabd6bbd2f28609fcab2cdc5a340a1b003
lavigupta88 <lavigupta8814@gmail.com>
2026-09-15 01:14:39 +0530

    Learned Components & Changed folder name of 1

---
+++ 02-Component/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './components/card.jsx';
+import Navbar from './components/Navbar.jsx';
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Card />
+    </div>
+  );
+};
+
+export default App;
---
+++ 02-Component/src/components/card.jsx
@@ -0,0 +1,13 @@
+
+const card = () => {
+  return (
+    <div className='card'>
+      <h1>Lalalavii</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere neque, placeat modi voluptatibus sed deleniti.</p>
+    </div>
+  )
+}
+
+export default card
+
+
